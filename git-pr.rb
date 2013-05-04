75728c5eb7d157fd8662fbf43d151536b37e4f28
Parker Moore <parkrmoore@gmail.com>
2013-05-04 17:14:31 +0200

    JK need that SHA-1

--- git-pr.rb
+++ git-pr.rb
@@ -3,6 +3,7 @@ require 'formula'
 class GitPr < Formula
   homepage 'https://github.com/parkr/git-pr'
   url 'https://github.com/parkr/git-pr/archive/v0.0.1.zip'
+  sha1 '14432b9a9fe24dc263a7d4bcb8335d6c2ce66b36'
 
   def install
     bin.install "git-pr"
