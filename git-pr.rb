886edfb3f73794bbc5817d0ba8b7688b99c77175
Parker Moore <parkrmoore@gmail.com>
2013-09-29 13:48:59 -0400

    New SHA-1 yet again

--- git-pr.rb
+++ git-pr.rb
@@ -3,7 +3,7 @@ require 'formula'
 class GitPr < Formula
   homepage 'https://github.com/parkr/git-pr'
   url 'https://github.com/parkr/git-pr/archive/v0.0.3.zip'
-  sha1 '36c4846134eb8da13df4a4049f9a9bf0ef37d4ce'
+  sha1 'be003b09f718295c5e83ac42f3b6ce6b00e54341'
 
   def install
     bin.install "git-pr"
